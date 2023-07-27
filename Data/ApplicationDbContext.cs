57f1476f7ca6d9ab277c3f3d74db978cdfbae6e3
irvinakocan <irvinakocan@gmail.com>
2023-07-27 21:20:54 +0200

    Definisan kontekst podataka

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentWebApplication.Models;
 
 namespace StudentWebApplication.Data
 {
@@ -8,6 +9,19 @@ namespace StudentWebApplication.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<UpisNaPredmet> UpisNaPredmet { get; set; }
+        public DbSet<Predmet> Predmet { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Student>().ToTable("Student");
+            builder.Entity<UpisNaPredmet>().ToTable("UpisNaPredmet");
+            builder.Entity<Predmet>().ToTable("Predmet");
+
+            base.OnModelCreating(builder);
         }
     }
 }
