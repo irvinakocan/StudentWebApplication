0f0488df29bdab05153130b6fb5851825e22216c
irvinakocan <irvinakocan@gmail.com>
2023-07-27 21:40:17 +0200

    Izvrsena migracija na cloud bazu smarterASP.net

--- Data/Migrations/20230727193220_PrvaMigracija.Designer.cs
+++ Data/Migrations/20230727193220_PrvaMigracija.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentWebApplication.Data;
 
@@ -11,9 +12,11 @@ using StudentWebApplication.Data;
 namespace StudentWebApplication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230727193220_PrvaMigracija")]
+    partial class PrvaMigracija
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
